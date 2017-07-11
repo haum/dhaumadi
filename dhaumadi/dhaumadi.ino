18332ee85472a8c350899e56911efd920ca85266
JackDesBwa <jackdesbwa@gmail.com>
2017-07-12 00:05:02 +0200

    arduino code for Léoké board

---
+++ dhaumadi/dhaumadi.ino
@@ -0,0 +1,44 @@
+int pins[] = {
+  A2, A3, A4, A5, A1, A0, 13
+};
+
+void setup() {
+  Serial.begin(115200);
+  for (auto pin : pins)
+    pinMode(pin, INPUT);
+}
+
+int getValue() {
+  int value = 0;
+  int v = 1;
+  for (auto pin : pins) {
+    if (digitalRead(pin))
+      value |= v;
+    v <<= 1;
+  }
+  return value;
+}
+
+void loop() {
+  int idx_a_zero = 0;
+  for (auto pin_a_zero : pins) {
+    pinMode(pin_a_zero, OUTPUT);
+    digitalWrite(pin_a_zero, 0);
+    
+    int value = getValue();
+    
+    pinMode(pin_a_zero, INPUT);
+    
+    Serial.print(idx_a_zero);
+    Serial.print(' ');
+    Serial.println(value);
+    
+    ++idx_a_zero;
+    delay(20);
+  }
+
+  int value = getValue();
+  Serial.print("- ");
+  Serial.println(value);
+  delay(20);
+}
