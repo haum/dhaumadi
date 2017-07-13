d405cb3757b2693db1b24f17c8b65b706dadf0fd
JackDesBwa <jackdesbwa@gmail.com>
2017-07-13 20:52:57 +0200

    display only groups of touches in python script

--- dhaumadi.py
+++ dhaumadi.py
@@ -4,8 +4,12 @@ import serial
 
 inhib = 0
 touches = {}
+oldgroups = set()
+groupstability = 0
+laststablegroups = set()
 
 ser = serial.Serial('/dev/ttyACM0', 115200)
+
 while True:
     line = ser.readline()
     if line == '':
@@ -16,13 +20,28 @@ while True:
     value = (~int(a[1][:-5])) & 0x7F
 
     if pad == '-':
+        groups = set()
+        for pad in touches:
+            pads = ''
+            i = 1
+            value = touches[pad]
+            while value > 0:
+                if value & 1:
+                    pads += str(i)
+                value >>= 1
+                i += 1
+            if len(pads) > 1:
+                groups.add(pads)
+        if groups != oldgroups:
+            oldgroups = groups
+            groupstability = 0
+        else:
+            groupstability += 1
+            if groupstability == 3 and laststablegroups != groups:
+                laststablegroups = groups
+                print(' '.join(groups))
         inhib = value
     else:
         value &= ~inhib
         value |= 1<<int(pad)
-
-    if not pad in touches or touches[pad][0] != value:
-        touches[pad] = [value, 0]
-    elif touches[pad][1] == 0:
-        print((pad, value))
-        touches[pad][1] = 1
+    touches[pad] = value
