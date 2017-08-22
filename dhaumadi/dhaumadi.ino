962c413245adbcd004698582a75c51538a35924e
JackDesBwa <jackdesbwa@gmail.com>
2017-08-22 21:40:37 +0200

    Update firmware

--- dhaumadi/dhaumadi.ino
+++ dhaumadi/dhaumadi.ino
@@ -1,11 +1,15 @@
-int pins[] = {
+const int pins[] = {
   A2, A3, A4, A5, A1, A0, 13
 };
 
 void setup() {
   Serial.begin(115200);
-  for (auto pin : pins)
+  for (auto pin : pins) {
+    pinMode(pin, OUTPUT);
+    digitalWrite(pin, HIGH);
     pinMode(pin, INPUT);
+    digitalWrite(pin, LOW);
+  }
 }
 
 int getValue() {
@@ -20,26 +24,28 @@ int getValue() {
 }
 
 void loop() {
-  const int dly = 5;
+  const int dly = 500;
   int idx_a_zero = 0;
   for (auto pin_a_zero : pins) {
     pinMode(pin_a_zero, OUTPUT);
-    digitalWrite(pin_a_zero, 0);
-    
+    delayMicroseconds(dly);
+
     int value = getValue();
-    
+
+    digitalWrite(pin_a_zero, HIGH);
     pinMode(pin_a_zero, INPUT);
-    
+    digitalWrite(pin_a_zero, LOW);
+
     Serial.print(idx_a_zero);
     Serial.print(' ');
     Serial.println(value);
-    
+
     ++idx_a_zero;
-    delay(dly);
   }
 
   int value = getValue();
   Serial.print("- ");
   Serial.println(value);
-  delay(dly);
+  delayMicroseconds(dly);
+  delay(50);
 }
